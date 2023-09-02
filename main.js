b06c46fc1bfa2380f98dd90b64b4cafb7a092265
キャッツウィズライス <103961848+Vaneeyo@users.noreply.github.com>
2023-09-02 14:37:53 +0200

    Main.js rewrite (v1.0.1)
    Big main.js rewrite and added settings

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ const fs = require('fs')
 const path = require('path');
 const { json } = require('body-parser');
 const { exit } = require('process');
-let settingsWindow = null;
+const { open } = require('fs/promises');
 const icon = __dirname + '/src/res/Icon.png'
 const iconTray = __dirname + '/src/res/IconTray.png'
 
+let settingsWindow = null;
+let musicWidget = null;
+let batteryWidget = null;
+let deviceCareWidget = null;
+let weatherWidget = null;
 
 const folderPath = path.join(os.homedir(), 'AppData', 'Local', 'OneUI-Widgets');
 
@@ -32,97 +37,142 @@ const positionData = {
     }
 };
 
+const stateData = {
+    musicWidget: {
+        show: "true"
+    },
+    batteryWidget: {
+        show: "true"
+    },
+    deviceCareWidget: {
+        show: "true"
+    },
+    weatherWidget: {
+        show: "true"
+    }
+};
+
 const positionJSON = JSON.stringify(positionData, null, 4);  // null and 4 for pretty formatting
 
 if (!fs.existsSync(folderPath + "\\widgetPositions.json")) fs.writeFileSync(folderPath + "\\widgetPositions.json", positionJSON)
 
+const stateJSON = JSON.stringify(stateData, null, 4);  // null and 4 for pretty formatting
+
+if (!fs.existsSync(folderPath + "\\widgetStates.json")) fs.writeFileSync(folderPath + "\\widgetStates.json", stateJSON)
+
 app.on('ready', () => {
     tray = new Tray(iconTray)
     const contextMenu = Menu.buildFromTemplate([
+        { label: 'Settings', click: () => { openSettings(); } },
+        { type: 'separator' },
         { role: 'quit' },
     ])
     tray.setToolTip('OneUI Windows')
     tray.setContextMenu(contextMenu)
 
-
-    musicWidget = new BrowserWindow({
-        width: 390,
-        height: 125,
-        frame: false,
-        transparent: true,
-        resizable: false,
-        skipTaskbar: true,
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
+    function openSettings() {
+        if (settingsWindow) {
+            // If the settings window already exists, focus on it
+            if (settingsWindow.isMinimized()) settingsWindow.restore();
+            settingsWindow.focus();
+            return;
         }
-    });
 
-    musicWidget.loadFile('./src/music.html');
+        // Create a new settings window
+        settingsWindow = new BrowserWindow({
+            width: 400,
+            height: 600,
+            webPreferences: {
+                contextIsolation: false,
+                nodeIntegration: true,
+            }
+        });
 
-    musicWidget.setIgnoreMouseEvents(true)
+        settingsWindow.loadFile('./src/settings.html'); // Load your HTML file here
 
-    batteryWidget = new BrowserWindow({
-        width: 390,
-        height: 125,
-        frame: false,
-        transparent: true,
-        resizable: false,
-        skipTaskbar: true,
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-        }
-    });
-
-    batteryWidget.loadFile('./src/battery.html');
-
-    batteryWidget.setIgnoreMouseEvents(true)
-
-    deviceCareWidget = new BrowserWindow({
-        width: 390,
-        height: 125,
-        frame: false,
-        transparent: true,
-        resizable: false,
-        skipTaskbar: true,
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-        }
-    });
-
-    deviceCareWidget.loadFile('./src/deviceCare.html');
-
-    deviceCareWidget.setIgnoreMouseEvents(true)
-
-    weatherWidget = new BrowserWindow({
-        width: 390,
-        height: 125,
-        frame: false,
-        transparent: true,
-        resizable: false,
-        skipTaskbar: true,
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-        }
-    });
-
-    weatherWidget.loadFile('./src/weather.html');
-
-    weatherWidget.setIgnoreMouseEvents(true)
-
-    function setPositions() {
-        const jsonData = JSON.parse(fs.readFileSync(folderPath + "\\widgetPositions.json", 'utf8'));
-        musicWidget.setPosition(parseInt(jsonData.musicWidget.x), parseInt(jsonData.musicWidget.y));
-        batteryWidget.setPosition(parseInt(jsonData.batteryWidget.x), parseInt(jsonData.batteryWidget.y));
-        deviceCareWidget.setPosition(parseInt(jsonData.deviceCareWidget.x), parseInt(jsonData.deviceCareWidget.y));
-        weatherWidget.setPosition(parseInt(jsonData.weatherWidget.x), parseInt(jsonData.weatherWidget.y));
+        settingsWindow.on('closed', () => {
+            settingsWindow = null;
+        });
     }
 
-    setPositions()
-    setInterval(setPositions, 3000)
+    const widgetsData = {
+        widgets: [
+            {
+                "name": "musicWidget",
+                "width": 390,
+                "height": 125,
+                "html": "./src/music.html"
+            },
+            {
+                "name": "batteryWidget",
+                "width": 390,
+                "height": 125,
+                "html": "./src/battery.html"
+            },
+            {
+                "name": "deviceCareWidget",
+                "width": 390,
+                "height": 125,
+                "html": "./src/deviceCare.html"
+            },
+            {
+                "name": "weatherWidget",
+                "width": 390,
+                "height": 125,
+                "html": "./src/weather.html"
+            }
+        ]
+    }
+
+    function setStates() {
+        const widgetStates = JSON.parse(fs.readFileSync(folderPath + "\\widgetStates.json"))
+        const widgetPositions = JSON.parse(fs.readFileSync(folderPath + "\\widgetPositions.json"))
+        widgetsData.widgets.forEach(widget => {
+            if (widgetStates[widget.name].show == "true" && eval(widget.name) == null) {
+                const widgetWindow = new BrowserWindow({
+                    width: widget.width,
+                    height: widget.height,
+                    frame: false,
+                    transparent: true,
+                    resizable: false,
+                    skipTaskbar: true,
+                    webPreferences: {
+                        contextIsolation: false,
+                        nodeIntegration: true,
+                    }
+                });
+
+                widgetWindow.webContents.openDevTools()
+
+                eval(`${widget.name} = widgetWindow`)
+
+                if (eval(widget.name) != null) {
+                    widgetWindow.setBounds({
+                        width: 390,
+                        height: 125,
+                        x: parseInt(widgetPositions[widget.name].x),
+                        y: parseInt(widgetPositions[widget.name].y)
+                    });
+                }
+
+                widgetWindow.loadFile(path.join(__dirname, widget.html));
+
+                widgetWindow.on('closed', () => {
+                    eval(`${widget.name} = null`);
+                    // Handle window closed event as needed
+                });
+            } else if (widgetStates[widget.name].show != "true" && eval(widget.name) != null) {
+                eval(`${widget.name}.destroy()`);
+                eval(`${widget.name} = null`);
+            }
+
+        });
+    }
+
+    setStates()
+    setInterval(function () {
+        setStates();
+    }, 500);
 });
 
 try {
---
+++ src/js/settings.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const folderPath = path.join(os.homedir(), 'AppData', 'Local', 'OneUI-Widgets');
+const jsonFilePath = path.join(folderPath, 'widgetStates.json');
+
+window.addEventListener('DOMContentLoaded', () => {
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+    function updateWidgetState(widgetName, checkbox) {
+        jsonData[widgetName].show = checkbox.checked.toString();
+        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 4), 'utf8');
+    }
+
+    const checkboxes = [
+        { name: 'musicWidget', element: document.getElementById('music-checkbox') },
+        { name: 'batteryWidget', element: document.getElementById('battery-checkbox') },
+        { name: 'deviceCareWidget', element: document.getElementById('deviceCare-checkbox') },
+        { name: 'weatherWidget', element: document.getElementById('weather-checkbox') },
+    ];
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.element.checked = jsonData[checkbox.name].show === 'true';
+
+        checkbox.element.addEventListener('change', (event) => {
+            updateWidgetState(checkbox.name, event.target);
+        });
+    });
+});
