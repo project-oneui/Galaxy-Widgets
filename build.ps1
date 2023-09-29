456cd75811112fb04e6488207f43b0d50ae56355
キャッツウィズライス <103961848+Vaneeyo@users.noreply.github.com>
2023-09-29 19:53:47 +0200

    build

--- build.ps1
+++ build.ps1
@@ -1,18 +1,2 @@
-# Define paths to candle.exe, light.exe, and Heat.exe
-$candlePath = "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe"
-$lightPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe"
-$heatPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe"
-
-# Set the source directory
-$sourceDir = "D:\a\oneui-desktop-widgets\oneui-desktop-widgets\samsung-widgets-win32-x64"
-
-# Compile the main WiX source file into .wixobj files
-& $candlePath "-o" "build/windows/" "samsung-widgets.wxs" "-dSourceDir=$sourceDir"
-
-# Harvest files from the specified directory using Heat and generate a WiX source file
-& $heatPath "dir" "$sourceDir" -dr INSTALLFOLDER -cg MyComponentGroup -gg -srd -sfrag -out "GeneratedFile.wxs"
-
-# Link the .wixobj files and generated .wxs file into an MSI installer
-& $lightPath "build\windows\samsung-widgets.wixobj", "GeneratedFile.wxs" "-o" "bin\samsung-widgets.msi"
-
-Write-Output "MSI Installer Created!"
+& "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" "-o" "build/windows/" "samsung-widgets.wxs" "-dSourceDir=D:\a\oneui-desktop-widgets\samsung-widgets\samsung-widgets-win32-x64"
+& "C:\Program Files (x86)\WiX Toolset v3.11\bin\light" "build\windows\*.wixobj" "-o" "bin\samsung-widgets.msi"
