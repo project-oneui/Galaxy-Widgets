d09bfd0b9dc8e44453c9b4a63572e87626b096d5
キャッツウィズライス <103961848+Vaneeyo@users.noreply.github.com>
2023-09-29 19:46:53 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ $lightPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe"
 $heatPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe"
 
 # Set the source directory
-$sourceDir = "D:\a\oneui-desktop-widgets\samsung-widgets\samsung-widgets-win32-x64"
+$sourceDir = "D:\a\oneui-desktop-widgets\oneui-desktop-widgets\samsung-widgets-win32-x64"
 
 # Compile the main WiX source file into .wixobj files
 & $candlePath "-o" "build/windows/" "samsung-widgets.wxs" "-dSourceDir=$sourceDir"
@@ -13,6 +13,6 @@ $sourceDir = "D:\a\oneui-desktop-widgets\samsung-widgets\samsung-widgets-win32-x
 & $heatPath "dir" "$sourceDir" -dr INSTALLFOLDER -cg MyComponentGroup -gg -srd -sfrag -out "GeneratedFile.wxs"
 
 # Link the .wixobj files and generated .wxs file into an MSI installer
-& $lightPath "build\windows\*.wixobj", "GeneratedFile.wxs" "-o" "bin\samsung-widgets.msi"
+& $lightPath "build\windows\samsung-widgets.wixobj", "GeneratedFile.wxs" "-o" "bin\samsung-widgets.msi"
 
 Write-Output "MSI Installer Created!"
