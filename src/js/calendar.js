56f55c963d05ba08d95cea12de16b722ab8ad657
キャッツウィズライス <103961848+Vaneeyo@users.noreply.github.com>
2023-10-19 16:04:01 +0200

    finally fixed contrast

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,29 +1,45 @@
 const os = require('os')
 const fs = require('fs')
 const path = require('path');
+const contrast = require('wcag-contrast')
+
 
 const folderPath = path.join(os.homedir(), 'AppData', 'Local', 'Samsung-Widgets');
 
 window.addEventListener("DOMContentLoaded", () => {
+    const containerMain = document.getElementById("container-main");
+
+    const secondaryColors = [
+        [179, 179, 179],
+        [142, 142, 142]
+    ];
+
+    const textColors = [
+        [0, 0, 0],
+        [250, 250, 250]
+    ];
+
     // change color based on Setting
     const colorData = JSON.parse(fs.readFileSync(path.join(folderPath, 'color.json'), 'utf8'));
 
-    const containerMain = document.getElementById("container-main");
-
-    const day = document.getElementsByClassName('day')
-    // Check its not black
-    containerMain.style.background = `linear-gradient(135deg, rgb(${colorData.red}, ${colorData.green}, ${colorData.blue}) 0%, rgb(${colorData.red - 35}, ${colorData.green - 35}, ${colorData.blue - 35}) 100%)`;
-
-    // check if text should be white or black
-    function getLuminance(r, g, b) {
-        return (r * 299 + g * 587 + b * 114) / 1000;
+    function findBetterContrast(rgb1, rgb2) {
+        const contrast1 = contrast.rgb(rgb1, [colorData.red, colorData.green, colorData.blue])
+        const contrast2 = contrast.rgb(rgb2, [colorData.red, colorData.green, colorData.blue])
+        if (contrast1 > contrast2) {
+            return `rgb(${rgb1[0]}, ${rgb1[1]}, ${rgb1[2]})`;
+        } else {
+            return `rgb(${rgb2[0]}, ${rgb2[1]}, ${rgb2[2]})`;
+        }
     }
 
-    const backgroundLuminance = getLuminance(colorData.red, colorData.green, colorData.blue);
-    const textColor = backgroundLuminance > 128 ? 'var(--text)' : 'black';
-    const secondaryColor = backgroundLuminance > 128 ? 'var(--secondary-lighter)' : 'var(--secondary-darker)';
+    const textColor = findBetterContrast(textColors[0], textColors[1])
+    const secondaryColor = findBetterContrast(secondaryColors[0], secondaryColors[1])
 
     containerMain.style.color = textColor;
+
+    const day = document.getElementsByClassName('day')
+
+    containerMain.style.background = `linear-gradient(135deg, rgb(${colorData.red}, ${colorData.green}, ${colorData.blue}) 0%, rgb(${colorData.red - 35}, ${colorData.green - 35}, ${colorData.blue - 35}) 100%)`;
 
     for (let i = 0; i < day.length; i++) {
         day[i].style.color = secondaryColor;
--- src/js/news.js
+++ src/js/news.js
@@ -1,6 +1,7 @@
 const os = require('os')
 const fs = require('fs')
 const path = require('path');
+const contrast = require('wcag-contrast')
 
 const folderPath = path.join(os.homedir(), 'AppData', 'Local', 'Samsung-Widgets');
 
@@ -10,16 +11,32 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const containerMain = document.getElementById("container-main");
 
-    // Check its not black
     containerMain.style.background = `linear-gradient(135deg, rgb(${colorData.red}, ${colorData.green}, ${colorData.blue}) 0%, rgb(${colorData.red - 35}, ${colorData.green - 35}, ${colorData.blue - 35}) 100%)`;
 
-    // check if text should be white or black
-    function getLuminance(r, g, b) {
-        return (r * 299 + g * 587 + b * 114) / 1000;
+    const secondaryColors = [
+        [179, 179, 179],
+        [142, 142, 142]
+    ];
+
+    const textColors = [
+        [0, 0, 0],
+        [250, 250, 250]
+    ];
+
+    // change color based on Setting
+
+    function findBetterContrast(rgb1, rgb2) {
+        const contrast1 = contrast.rgb(rgb1, [colorData.red, colorData.green, colorData.blue])
+        const contrast2 = contrast.rgb(rgb2, [colorData.red, colorData.green, colorData.blue])
+        if (contrast1 > contrast2) {
+            return `rgb(${rgb1[0]}, ${rgb1[1]}, ${rgb1[2]})`;
+        } else {
+            return `rgb(${rgb2[0]}, ${rgb2[1]}, ${rgb2[2]})`;
+        }
     }
 
-    const backgroundLuminance = getLuminance(colorData.red, colorData.green, colorData.blue);
-    const textColor = backgroundLuminance > 128 ? 'var(--text)' : 'black';
+    const textColor = findBetterContrast(textColors[0], textColors[1])
+    const secondaryColor = findBetterContrast(secondaryColors[0], secondaryColors[1])
 
     containerMain.style.color = textColor;
 
